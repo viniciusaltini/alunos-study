528faa82544dcb829c631d41d48a1cc4828dab1c
Altini <vinicius_lima152@estudante.sesisenai.org.br>
2025-06-10 16:56:23 -0300

    second commit

--- src/alunos/resolucao.java
+++ src/alunos/resolucao.java
@@ -11,14 +11,26 @@ public class resolucao {
 		String vetSituacao [] = new String [6]; //>= 7 = aprovado; 5 até 6.99 = em recuperação; < 5 = reprovado
 		double matNotas[][] = new double[6][3]; //6 alunos, 3 notas para cada
 		double acumNotas = 0;
+		int contaNotas = 0;
+		double maiorMedia = 0;
+		double menorMedia = 0;
 		for (int lin = 0; lin < 6; lin++) {
-			
+			acumNotas = 0;
 			for (int col = 0; col < 3; col++) {
 				matNotas[lin][col] = Double.valueOf(JOptionPane.showInputDialog("Informe a " + (col+1) + "ª nota do " + (lin+1) + "º aluno"));
 				acumNotas += matNotas[lin][col];
+				if (matNotas [lin][col] < 7.5) {
+					contaNotas++;
+				}
 			}
+			menorMedia = matNotas[0][0];
 			vetMedia[lin] = Math.round(acumNotas/3);
 			vetSituacao [lin] = testaSituacao(vetMedia[lin]);
+			if (vetMedia[lin] > maiorMedia) {
+				maiorMedia = vetMedia[lin];
+			} else if (vetMedia[lin] < menorMedia) {
+				menorMedia = vetMedia[lin];
+			}
 		}
 		String saida = "As notas do(a) aluno(a) " + vetNomes[0] + " são " + matNotas[0][0] + " " + matNotas[0][1] + " " + matNotas[0][2] + " "
 				+ "e sua média é " + vetMedia[0] + " e ele(a) está " + vetSituacao[0] + "\nAs notas do(a) aluno(a) " + vetNomes[1] + " são " + matNotas[1][0] + " " + matNotas[1][1] + " " + matNotas[1][2] + " "
@@ -26,7 +38,7 @@ public class resolucao {
 								+ "e sua média é " + vetMedia[2] + " e ele(a) está " + vetSituacao[2] + "\nAs notas do(a) aluno(a) " + vetNomes[3] + " são " + matNotas[3][0] + " " + matNotas[3][1] + " " + matNotas[3][2] + " "
 										+ "e sua média é " + vetMedia[3] + " e ele(a) está " + vetSituacao[3] + "\nAs notas do(a) aluno(a) " + vetNomes[4] + " são " + matNotas[4][0] + " " + matNotas[4][1] + " " + matNotas[4][2] + " "
 												+ "e sua média é " + vetMedia[4] + " e ele(a) está " + vetSituacao[4] + "\nAs notas do(a) aluno(a) " + vetNomes[5] + " são " + matNotas[5][0] + " " + matNotas[5][1] + " " + matNotas[5][2] + " "
-														+ "e sua média é " + vetMedia[5] + " e ele(a) está " + vetSituacao[5];
+														+ "e sua média é " + vetMedia[5] + " e ele(a) está " + vetSituacao[5] + "\nA quantidade de notas abaixo de 7.5 é " + contaNotas + " a menor média foi " + menorMedia + " e a maior média foi " + maiorMedia;
 		JOptionPane.showMessageDialog(null, saida);
 	}
 	
